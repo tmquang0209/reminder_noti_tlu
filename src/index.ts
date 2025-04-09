0abb4935502a424f3045befc69cd7277c2177d43
Trần Minh Quang <tmquang0209@gmail.com>
2025-04-09 15:39:58 +0700

    refactor: simplify dotenv configuration import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import express from "express";
 import config from "./config";
-dotenv.config();
+
 const app = express();
 
 app.post("/webhook", (req, res) => {
